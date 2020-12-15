0a8c7616e374e023f15f32f100dbec5d2a47f6dd
Aleksandra <aleksandrasovawork@gmail.com>
2020-12-15 20:16:48 +0300

    Lesson03

---
+++ src/Main/Lesson03/Apple.java
@@ -0,0 +1,7 @@
+package Main.Lesson03;
+
+public class Apple extends Fruit{
+    public Apple() {
+        super(1f);
+    }
+}
---
+++ src/Main/Lesson03/Orange.java
@@ -0,0 +1,7 @@
+package Main.Lesson03;
+
+public class Orange extends Fruit{
+    public Orange() {
+        super(1.5f);
+    }
+}
